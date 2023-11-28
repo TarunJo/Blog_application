61d838c266572a202d7c1bed33931db3b64f4309
Tarun Joshi <tarun.joshi.26.1@mountblue.tech>
2023-11-28 10:10:16 +0530

    security bug fixes

--- src/main/java/com/mountblue/spring/blogApplication/security/SecurityConfiguration.java
+++ src/main/java/com/mountblue/spring/blogApplication/security/SecurityConfiguration.java
@@ -43,8 +43,7 @@ public class SecurityConfiguration {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests(
                         configurer -> configurer
-                                .requestMatchers("/css/**", "/register**", "/adduser").permitAll()
-                                .requestMatchers("/**").hasAnyRole("author","admin")
+                                .requestMatchers("/**").permitAll()
                                 .anyRequest().authenticated()
                 )
                 .exceptionHandling(configurer ->
